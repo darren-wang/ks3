7077e7932be69c9b699eb1041e7ed70bf8893bdc
darren-wang <darren_wang@outlook.com>
2015-05-16 00:06:51 +0800

    add 'description' to service DB table

---
+++ keystone/common/sql/migrate_repo/versions/019_add_description_to_service.py
@@ -0,0 +1,13 @@
+import sqlalchemy as sql
+
+_SERVICE_TABLE_NAME = 'service'
+
+def upgrade(migrate_engine):
+    meta = sql.MetaData()
+    meta.bind = migrate_engine
+
+    service_table = sql.Table(_SERVICE_TABLE_NAME, meta, autoload=True)
+
+    # adding new columns
+    service_desc = sql.Column('description', sql.String(255), nullable=False)
+    service_table.create_column(service_desc)
