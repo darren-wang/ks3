83ccc64c7826febf2a0e6c10a2102ca9c699264f
wzy <wzy@wzy-pc>
2015-06-25 01:03:31 +0800

    add role_hierarchy table. (without foreign key constraints)

--- keystone/common/sql/migrate_repo/versions/026_add_role_hierarchy.py
+++ keystone/common/sql/migrate_repo/versions/026_add_role_hierarchy.py
@@ -10,18 +10,12 @@ def upgrade(migrate_engine):
     meta = sql.MetaData()
     meta.bind = migrate_engine
 
-    mapping_table = sql.Table(
+    rh_table = sql.Table(
         HIERARCHY_TABLE,
         meta,
-        sql.Column('public_id', sql.String(64), primary_key=True),
-        sql.Column('domain_id', sql.String(64), nullable=False),
-        sql.Column('local_id', sql.String(64), nullable=False),
-        sql.Column('entity_type', sql.Enum(
-            mapping.EntityType.USER,
-            mapping.EntityType.GROUP,
-            name='entity_type'),
-            nullable=False),
-        sql.UniqueConstraint('domain_id', 'local_id', 'entity_type'),
+        sql.Column('asc_id', sql.String(64), nullable=False),
+        sql.Column('desc_id', sql.String(64), nullable=True),
         mysql_engine='InnoDB',
         mysql_charset='utf8')
-    mapping_table.create(migrate_engine, checkfirst=True)
+
+    rh_table.create(migrate_engine, checkfirst=True)
