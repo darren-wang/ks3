b1714dfb4439f2062e2455eab56f2e3044a94aad
darren_wang <darren_wang@outlook.com>
2015-10-19 17:20:59 +0800

    remove identity mapping dependency on keystoneclient

--- keystone/policy/core.py
+++ keystone/policy/core.py
@@ -11,8 +11,6 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-from openstackclient.identity.v3.mapping import _RulesReader
-
 """Main entry point into the Policy service."""
 
 import abc
